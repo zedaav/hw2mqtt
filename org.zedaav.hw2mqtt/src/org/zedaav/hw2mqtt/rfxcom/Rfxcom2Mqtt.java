2dda21a925b80953011df362a7daf6a7583671ba
David Pochet <dpochet@sierrawireless.com>
2019-02-02 17:54:48 +0100

    RFXCOM reconnect (on power failure)

--- org.zedaav.hw2mqtt/src/org/zedaav/hw2mqtt/rfxcom/Rfxcom2Mqtt.java
+++ org.zedaav.hw2mqtt/src/org/zedaav/hw2mqtt/rfxcom/Rfxcom2Mqtt.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.InterruptedIOException;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Properties;
@@ -20,60 +21,105 @@ public class Rfxcom2Mqtt implements Hw2MqttService {
 
 	private static final String RFXCOM_HOST = "rfxcom.host";
 	private static Rfxcom2Mqtt instance;
-	
+
 	private String host;
-	private InputStream in = null;
-	private OutputStream out = null;
-	private Socket socket = null;
 	private Thread readerThread = null;
 	private SimpleLogger logger = SimpleLogger.getInstance();
-	
+
 	public static Rfxcom2Mqtt getInstance() {
 		if (instance == null) {
 			instance = new Rfxcom2Mqtt();
 		}
 		return instance;
 	}
-	
+
 	public void init(Properties props) throws IOException {
 		// Check Rfxcom IP
 		host = PropertiesReader.getProperty(RFXCOM_HOST, props);
 	}
-	
+
 	public void start() throws IOException {
-		socket = new Socket(host, 10001);
-		in = socket.getInputStream();
-		out = socket.getOutputStream();
-
-		out.flush();
-		if (in.markSupported()) {
-			in.reset();
-		}
-
 		// Run reader
-		readerThread = new SocketReader(in);
+		readerThread = new SocketReader(host, 10001);
 		readerThread.start();
 	}
-	
+
 	private class SocketReader extends Thread {
 		boolean interrupted = false;
 		InputStream in;
+		OutputStream out;
+		Socket socket;
+		String host;
+		int port;
 
-		public SocketReader(InputStream in) {
-			this.in = in;
+		public SocketReader(String host, int port) {
+			this.host = host;
+			this.port = port;
+		}
+
+		public void connect() throws IOException {
+			logger.log(LoggerLevel.INFO, "Connecting to RFXCOM: %s %d", this.host, this.port);
+			this.socket = new Socket(this.host, this.port);
+			this.in = socket.getInputStream();
+			this.out = socket.getOutputStream();
+			socket.setSoTimeout(60*1000);
+
+			out.flush();
+			if (this.in.markSupported()) {
+				this.in.reset();
+			}
+		}
+
+		public void disconnect() {
+			try {
+				this.in.close();
+				this.out.close();
+				this.socket.close();
+			} catch (IOException e) {
+			} // quietly close
 		}
 
 		@Override
 		public void interrupt() {
 			interrupted = true;
 			super.interrupt();
-			try {
-				in.close();
-			} catch (IOException e) {
-			} // quietly close
+			disconnect();
 		}
 
 		public void run() {
+			logger.log(LoggerLevel.DEBUG, "Data listener started");
+			
+			// Loop to reconnect in case of unexpected disconnection
+			// (e.g. power cut)
+			while (true) {
+				try {
+					// Connect to RFXCOM
+					connect();
+					
+					// Handle data read
+					readLoop();
+				} catch (SocketTimeoutException e) {
+					logger.error("Read timeout");
+				} catch (InterruptedIOException e) {
+					Thread.currentThread().interrupt();
+					logger.error("Interrupted via InterruptedIOException");
+				} catch (IOException e) {
+					logger.error("Reading from socket failed", e);
+				}
+				
+				// Clean any pending connection
+				disconnect();
+				
+				// Restart after some time
+				try {
+					sleep(1000);
+				} catch (InterruptedException e) {
+					// Fails silently
+				}
+			}
+		}
+
+		private void readLoop() throws IOException, SocketTimeoutException {
 			final int dataBufferMaxLen = Byte.MAX_VALUE;
 
 			byte[] dataBuffer = new byte[dataBufferMaxLen];
@@ -81,63 +127,60 @@ public class Rfxcom2Mqtt implements Hw2MqttService {
 			int msgLen = 0;
 			boolean start_found = false;
 
-			logger.log(LoggerLevel.DEBUG, "Data listener started");
+			byte[] tmpData = new byte[20];
+			int len = -1;
 
-			try {
+			while (!interrupted) {
+				len = in.read(tmpData);
+				if (len < 0) {
+					logger.log(LoggerLevel.ERROR, "Error while reading from socket");
+					break;
+				}
 
-				byte[] tmpData = new byte[20];
-				int len = -1;
+				byte[] logData = Arrays.copyOf(tmpData, len);
+				logger.log(LoggerLevel.DEBUG, "Received data (len=%d): %s", len, logger.printHexBinary(logData));
 
-				while ((len = in.read(tmpData)) > 0 && !interrupted) {
+				// Just one byte received? --> start of the frame
+				if (len == 1) {
+					// We're OK to go with the next read. This is the frame length (in bits)
+					start_found = true;
+					dataBuffer[0] = tmpData[0];
+					logger.log(LoggerLevel.DEBUG, "Start of frame detected");
+				} else if (start_found) {
+					// Validate length
+					if ((dataBuffer[0] <= len * 8) && (dataBuffer[0] > (len - 1) * 8)) {
+						// Remember the length
+						msgLen = len + 1;
 
-					byte[] logData = Arrays.copyOf(tmpData, len);
-					logger.log(LoggerLevel.DEBUG, "Received data (len=%d): %s", len, logger.printHexBinary(logData));
+						// Copy the frame
+						System.arraycopy(tmpData, 0, dataBuffer, 1, len);
 
-					// Just one byte received? --> start of the frame
-					if (len == 1) {
-						// We're OK to go with the next read. This is the frame length (in bits)
-						start_found = true;
-						dataBuffer[0] = tmpData[0];
-						logger.log(LoggerLevel.DEBUG, "Start of frame detected");
-					} else if (start_found) {
-						// Validate length
-						if ((dataBuffer[0] <= len*8) && (dataBuffer[0] > (len-1)*8)) {
-							// Remember the length
-							msgLen = len+1;
-	
-							// Copy the frame
-							System.arraycopy(tmpData, 0, dataBuffer, 1, len);
-	
-							// whole message received, send an event
-							byte[] msg = new byte[msgLen + 1];
-	
-							for (int j = 0; j < msgLen; j++) {
-								msg[j] = dataBuffer[j];
-							}
-	
-							// Full frame event
-							logger.log(LoggerLevel.DEBUG, "Full frame detected (len=%d): %s", msgLen, logger.printHexBinary(msg));
-							processFrame(msg);
-						} else {
-							logger.log(LoggerLevel.DEBUG, "Invalid frame length: %d: was expecting %d", len*8, dataBuffer[0]);
+						// whole message received, send an event
+						byte[] msg = new byte[msgLen + 1];
+
+						for (int j = 0; j < msgLen; j++) {
+							msg[j] = dataBuffer[j];
 						}
 
-						// find new start
-						start_found = false;
+						// Full frame event
+						logger.log(LoggerLevel.DEBUG, "Full frame detected (len=%d): %s", msgLen,
+								logger.printHexBinary(msg));
+						processFrame(msg);
 					} else {
-						// Garbage data, don't know what to do with it
-						logger.log(LoggerLevel.DEBUG, "Unknown data");
+						logger.log(LoggerLevel.DEBUG, "Invalid frame length: %d: was expecting %d", len * 8,
+								dataBuffer[0]);
 					}
+
+					// find new start
+					start_found = false;
+				} else {
+					// Garbage data, don't know what to do with it
+					logger.log(LoggerLevel.DEBUG, "Unknown data");
 				}
-			} catch (InterruptedIOException e) {
-				Thread.currentThread().interrupt();
-				logger.error("Interrupted via InterruptedIOException");
-			} catch (IOException e) {
-				logger.error("Reading from socket failed", e);
 			}
 		}
 	}
-	
+
 	private void processFrame(byte[] msg) {
 		// Check for length
 		if ((msg[0] == 34) && (msg[5] == 0)) {
@@ -158,10 +201,10 @@ public class Rfxcom2Mqtt implements Hw2MqttService {
 		if (on_off) {
 			deviceID = deviceID - 16;
 		}
-		String onOffPayload = on_off?Hw2MqttConstants.ON:Hw2MqttConstants.OFF;
+		String onOffPayload = on_off ? Hw2MqttConstants.ON : Hw2MqttConstants.OFF;
 		String deviceIDStr = String.format("%X", deviceID);
 		logger.log(LoggerLevel.INFO, "CHACON event: %s %s", deviceIDStr, onOffPayload);
-		
+
 		// Publish event
 		MqttConnector.getInstance().publish("chacon/" + deviceIDStr, onOffPayload);
 	}
